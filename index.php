11040653947067a5fba47f978004b56e692d1635
champseo2017 <32265715+champseo2017@users.noreply.github.com>
2018-10-21 20:13:05 +0700

    Update index.php

--- index.php
+++ index.php
@@ -3,8 +3,12 @@ $api_key="_3lKtZAAgeiBKfwitPECdGedVSAecR8g";
 $url = 'https://api.mlab.com/api/1/databases/line_bot/collections/line_bot?apiKey='.$api_key.'';
 $json = file_get_contents('https://api.mlab.com/api/1/databases/line_bot/collections/line_bot?apiKey='.$api_key);
 $data = json_decode($json,true);
+$g = [];
 foreach($data as $da){
 
-    print_r($da);
+    $g = $da->question;
+    print_r($g);
+
+    
 
 }
