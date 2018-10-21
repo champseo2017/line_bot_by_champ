01840fca2990ac5ed92b0ca6e292a2b3e4163e95
champseo2017 <32265715+champseo2017@users.noreply.github.com>
2018-10-21 20:38:12 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -16,6 +16,8 @@ $json = file_get_contents('https://api.mlab.com/api/1/databases/line_bot/collect
 $data = json_decode($json);
 $isData=sizeof($data);
 
+
+
 if (strpos($_msg, 'สอนบอท') !== false) {
   if (strpos($_msg, 'สอนบอท') !== false) {
     $x_tra = str_replace("สอนบอท","", $_msg);
@@ -70,4 +72,33 @@ curl_setopt($channel, CURLOPT_RETURNTRANSFER,true);
 curl_setopt($channel, CURLOPT_SSL_VERIFYPEER, false);
 $result = curl_exec($channel);
 curl_close ($channel);
+
+
+ //รับข้อความจากผู้ใช้
+ $_msg = $arrayJson['events'][0]['message']['text'];
+ //รับ id ของผู้ใช้
+ $id = $arrayJson['events'][0]['source']['userId'];
+ #ตัวอย่าง Message Type "Text + Sticker"
+ if($_msg == "สวัสดี"){
+    $arrayPostData['to'] = $id;
+    $arrayPostData['messages'][0]['type'] = "text";
+    $arrayPostData['messages'][0]['text'] = "สวัสดีจ้าาา";
+    $arrayPostData['messages'][1]['type'] = "sticker";
+    $arrayPostData['messages'][1]['packageId'] = "2";
+    $arrayPostData['messages'][1]['stickerId'] = "34";
+    pushMsg($arrayHeader,$arrayPostData);
+ }
+ function pushMsg($arrayHeader,$arrayPostData){
+    $strUrl = "https://api.line.me/v2/bot/message/push";
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_URL,$strUrl);
+    curl_setopt($ch, CURLOPT_HEADER, false);
+    curl_setopt($ch, CURLOPT_POST, true);
+    curl_setopt($ch, CURLOPT_HTTPHEADER, $arrayHeader);
+    curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($arrayPostData));
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER,true);
+    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+    $result = curl_exec($ch);
+    curl_close ($ch);
+ }
 ?>
