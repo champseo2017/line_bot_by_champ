a1dd7a2b9e683102cf9ec7da4976446ff8078c5e
champseo2017 <32265715+champseo2017@users.noreply.github.com>
2018-10-21 21:02:19 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -31,10 +31,10 @@ if($_msg == "สวัสดี"){
 function pushMsg($arrHeader,$arrayPostData){
    $strUrl2 = "https://api.line.me/v2/bot/message/push";
    $ch = curl_init();
-   curl_setopt($ch, CURLOPT_URL,$strUrl);
+   curl_setopt($ch, CURLOPT_URL,$strUrl2);
    curl_setopt($ch, CURLOPT_HEADER, false);
    curl_setopt($ch, CURLOPT_POST, true);
-   curl_setopt($ch, CURLOPT_HTTPHEADER, $arrayHeader);
+   curl_setopt($ch, CURLOPT_HTTPHEADER, $arrHeader);
    curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($arrayPostData));
    curl_setopt($ch, CURLOPT_RETURNTRANSFER,true);
    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
