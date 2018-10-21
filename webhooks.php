e71ac5a075d87a2a1fe56f8db2df850e74746a66
champseo2017 <32265715+champseo2017@users.noreply.github.com>
2018-10-21 17:24:10 +0700

    Update webhooks.php

--- webhooks.php
+++ webhooks.php
@@ -33,6 +33,7 @@ if (!is_null($events['events'])) {
 				'messages' => [$messages],
 			];
 			$post = json_encode($data);
+			print_r($post);
 			$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
 
 			$ch = curl_init($url);
