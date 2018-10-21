862825967251f7527b0af8294232b243b6b5f3a8
champseo2017 <32265715+champseo2017@users.noreply.github.com>
2018-10-21 20:48:02 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,10 +1,2 @@
 <?php
-$api_key="_3lKtZAAgeiBKfwitPECdGedVSAecR8g";
-$url = 'https://api.mlab.com/api/1/databases/line_bot/collections/line_bot?apiKey='.$api_key.'';
-$json = file_get_contents('https://api.mlab.com/api/1/databases/line_bot/collections/line_bot?apiKey='.$api_key);
-
-print_r($json);
-?>
-<script>
-console.log("dwd");
-</script>
+echo "ewewewe";
