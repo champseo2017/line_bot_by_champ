26ef6b9c5422b2af480b28552bca7c72f627c71a
champseo2017 <32265715+champseo2017@users.noreply.github.com>
2018-10-21 20:00:34 +0700

    Update index.php

--- index.php
+++ index.php
@@ -2,6 +2,6 @@
 $api_key="_3lKtZAAgeiBKfwitPECdGedVSAecR8g";
 $url = 'https://api.mlab.com/api/1/databases/line_bot/collections/line_bot?apiKey='.$api_key.'';
 $json = file_get_contents('https://api.mlab.com/api/1/databases/line_bot/collections/line_bot?apiKey='.$api_key);
-$data = json_decode($json);
+$data = json_decode($json,true);
 print_r($data);
 echo "Hello LINE BOT";
