61bc8d4a85aa1c3a1d6821da6f2dc94420842c23
champseo2017 <32265715+champseo2017@users.noreply.github.com>
2018-10-21 18:39:05 +0700

    Update webhooks.php

--- webhooks.php
+++ webhooks.php
@@ -41,9 +41,10 @@ if (!is_null($events['events'])) {
 			curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
 			curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
 			curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
-		
+			$result = curl_exec($ch);
+			curl_close($ch);
 
-		
+			echo "เราได้รับข้อความของคุณแล้ว";
 		}
 	}
 }
